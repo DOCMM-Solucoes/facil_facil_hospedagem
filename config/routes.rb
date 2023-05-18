c3ed7fcff589dc2685c27873ccc85bf36729fcf1
Cassio Santana <cassioo.roger@gmail.com>
2023-05-18 01:43:46 -0300

    build: Update routes
    Ref #3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :addresses
-  resources :hoteliers
+  resources :hoteliers do
+    resource :address
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
