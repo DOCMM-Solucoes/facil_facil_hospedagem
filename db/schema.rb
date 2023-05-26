938b4bbf2ecc66adeb439d6c1826be43f750bee4
Osiris Mariano <oslimacontato@gmail.com>
2023-05-26 14:39:26 -0300

    feat: guide schema feat #4

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_26_173033) do
+  create_table "guides", force: :cascade do |t|
+    t.string "guide"
+    t.string "phone"
+    t.string "cpf"
+    t.string "enterprise"
+    t.string "cnpj"
+    t.string "city"
+    t.string "uf"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
