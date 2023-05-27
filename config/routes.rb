825cb1232210a86a48d2728ae7ded448b9f317c9
Osiris Mariano <oslimacontato@gmail.com>
2023-05-27 15:27:14 -0300

    feat: guide routes ref: #4

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :guides
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  scope '(locale)', locale:/pt-BR|en/ do
+    resources :guides
+  end
 end
