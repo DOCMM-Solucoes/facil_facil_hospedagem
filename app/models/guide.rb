90aea44eb3f7ab6da5333d12ae5319e4a90c585f
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-06-06 12:36:29 -0300

    feat: guides establishments associations

--- app/models/guide.rb
+++ app/models/guide.rb
@@ -1,5 +1,5 @@
 class Guide < ApplicationRecord
   has_one :address,:as =>:addressable ,dependent: :destroy
-
+  has_and_belongs_to_many :establishments
   accepts_nested_attributes_for :address
 end
