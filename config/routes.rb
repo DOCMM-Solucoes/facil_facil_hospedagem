615880464390273f1caa6121816aa078de1a18aa
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-06-08 12:00:51 -0300

    feat(trip): route configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :trips
   devise_for :users
   root 'pages#home'
   scope '(locale)', locale:/pt-BR|en/ do
-      resources :guides
+    resources :guides
     resources :establishments
     resources :hoteliers
+    resources :trips
   end
 end
