c2adc0aee6a3784c1b3388bee6ea812ac0c4ef72
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-06-09 11:03:09 -0300

    feat(trip): association with trip

--- app/models/establishment.rb
+++ app/models/establishment.rb
@@ -2,4 +2,5 @@ class Establishment < ApplicationRecord
   has_one :address,:as =>:addressable ,dependent: :destroy
   belongs_to :hotelier
   accepts_nested_attributes_for :address
+  has_many :trips
 end
