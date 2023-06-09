33e6eadc1c8b1a37a33396f66c2d2fd87273bdb2
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-06-09 10:31:51 -0300

    feat: removal of direct association with 'guide'

--- app/models/establishment.rb
+++ app/models/establishment.rb
@@ -1,6 +1,5 @@
 class Establishment < ApplicationRecord
   has_one :address,:as =>:addressable ,dependent: :destroy
   belongs_to :hotelier
-  has_and_belongs_to_many :guides
   accepts_nested_attributes_for :address
 end
