28827098460afd4c3c6c22e92611425e6b520602
Sergio Oseko <osk.sergio@gmail.com>
2023-06-13 16:45:07 -0300

    feat: feature implemented to associate multiple photos of the rooms

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -68,6 +68,6 @@ class RoomsController < ApplicationController
       params.require(:room).permit(:name, :description, :reference, :maximum_capacity, :status, :photo_1, :photo_2, :photo_3, :photo_4, 
                                    :is_double, :is_triple, :is_quadruple, :is_quintuple, :is_sextuple, :is_single, :is_couple, 
                                    :is_couple_plus_one, :is_couple_plus_two, :is_couple_plus_three, :is_couple_plus_four, 
-                                   :is_two_couples, :is_two_couples_plus_one, :is_two_couples_plus_two, :is_three_couples)
+                                   :is_two_couples, :is_two_couples_plus_one, :is_two_couples_plus_two, :is_three_couples, images: [])
     end
 end
