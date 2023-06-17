73338da98e618054c6e0f6294649eadd868f4865
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-06-17 14:29:26 -0300

    fix: routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :photos
-  devise_for :users
-  root 'pages#home'
   scope '(locale)', locale:/pt-BR|en/ do
+    devise_for :users
+    root 'pages#home'
+    resources :photos
     resources :guides
     resources :establishments
     resources :hoteliers
