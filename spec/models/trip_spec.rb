a546b5dbfd7cd7cb8e53c8d8fe6a0bf58ac572df
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-06-22 22:45:13 -0300

    test(trip): validations, associations and factories

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -1,8 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Trip, type: :model do
+  describe 'validations' do
+    let(:trip) { FactoryBot.build(:trip) }
+
+    it 'validates presence of establishment_id' do
+      trip.establishment_id = nil
+      expect(trip).not_to be_valid
+    end
+
+    it 'validates presence of guide_id' do
+      trip.guide_id = nil
+      expect(trip).not_to be_valid
+    end
+  end
+
   describe 'associations' do
-    it { should belong_to(:guide) }
     it { should belong_to(:establishment) }
+    it { should belong_to(:guide) }
+  end
+
+  describe 'factories' do
+    it 'has a valid factory' do
+      trip = FactoryBot.build(:trip)
+      expect(trip).to be_valid
+    end
   end
 end
