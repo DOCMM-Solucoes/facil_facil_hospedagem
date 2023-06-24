be6b076d33812a9a0f216b870ba408054e11ecc3
Sergio Oseko <osk.sergio@gmail.com>
2023-06-24 06:59:55 -0300

    test: implemented the factory test

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,17 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
-  context "CRUD Room" do
-    it 'Create Room' do
-      room_type = Room.create(name: 'Room 101', description: 'Room 101', reference: 'Primeiro andar', maximum_capacity: 6, status: 'A')
-      expect(room_type.name).to eq('Room 101')
-      expect(room_type.reference).to eq('Primeiro andar')
-    end
 
-    it 'Update: modifying the name field' do
-      room_type = Room.create(name: 'Room 101', description: 'Room 101', reference: 'Primeiro andar', maximum_capacity: 6, status: 'A')
-      room_type.update(name: 'Room 1')
-      expect(room_type.name).to eq('Room 1')
+  describe 'Factory tests' do
+    context 'When using factory standard' do
+      it 'Is valid' do
+        expect(build(:room)).to be_valid
+      end
+    end
+  end
+
+  describe 'Room crud tests' do
+    context 'Create, update and delete tests' do
+      it 'Create room' do
+        room_type = Room.create(name: 'Room 101', description: 'Room 101', reference: 'Primeiro andar', maximum_capacity: 6, status: 'A')
+        expect(room_type.name).to eq('Room 101')
+        expect(room_type.reference).to eq('Primeiro andar')
+      end
+
+      it 'Update: modifying the name field' do
+        room_type = Room.create(name: 'Room 101', description: 'Room 101', reference: 'Primeiro andar', maximum_capacity: 6, status: 'A')
+        room_type.update(name: 'Room 1')
+        expect(room_type.name).to eq('Room 1')
+      end
     end
   end
 end
