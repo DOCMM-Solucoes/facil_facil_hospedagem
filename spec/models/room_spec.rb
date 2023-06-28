aa3219ebe3c34f7e24a26bf58b13bb384d4f338f
Sergio Oseko <osk.sergio@gmail.com>
2023-06-27 22:46:47 -0300

    test: implemented more model room tests

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -12,12 +12,23 @@ RSpec.describe Room, type: :model do
 
   describe 'Room crud tests' do
     context 'Create, update and delete tests' do
-      it 'Create room' do
+      it 'Create room using factory' do
         room_type = build(:room)
         expect(room_type.status).to eq('A')
         expect(room_type.is_double).to be_truthy
         expect(room_type.maximum_capacity).to eq(6)
       end
+
+      it 'Creates a new room' do
+        expect {
+          Room.create(name: 'Room Name', description: 'Lorem ipsum', reference: 'Room Ref.')
+        }.to change(Room, :count).by(1)
+      end
+
+      it 'Reads an existing room' do
+        room_type = create(:room)
+        expect(Room.find(room_type.id)).to eq(room_type)
+      end      
 
       it 'Update: modifying name, max. capacity and is_double' do
         room_type = build(:room)
@@ -27,11 +38,27 @@ RSpec.describe Room, type: :model do
         expect(room_type.maximum_capacity).to be(4)
       end
 
+      it 'Updates an existing room' do
+        room_type = create(:room)
+        new_description = 'Updated Description'
+        room_type.update(description: new_description)
+  
+        expect(room_type.reload.description).to eq(new_description)
+      end      
+
       it 'Delete room' do
         room_type = build(:room)
         room_type.destroy
         expect{ room_type.reload }.to raise_error(ActiveRecord::RecordNotFound)
       end
+
+      it 'Deletes an existing room' do
+        room_type = create(:room)
+  
+        expect {
+          room_type.destroy
+        }.to change(Room, :count).by(-1)
+      end      
     end
   end
 end
