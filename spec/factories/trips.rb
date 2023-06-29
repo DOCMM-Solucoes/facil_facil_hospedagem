aede2822a587898f476279c20be66b75f0c679c9
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-06-29 17:54:48 -0300

    refactor(trip): random data generation deletion

--- spec/factories/trips.rb
+++ spec/factories/trips.rb
@@ -2,6 +2,5 @@ FactoryBot.define do
   factory :trip do
     association :establishment
     association :guide
-    checkin_date { FFaker::Time.between(Date.today, Date.today + 1.year).to_date }
   end
 end
