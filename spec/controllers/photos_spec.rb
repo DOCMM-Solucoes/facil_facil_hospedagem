c2a41a00dcd3d69003be2387fff43bb3d932c41d
Osiris Mariano <oslimacontato@gmail.com>
2023-07-01 20:19:19 -0300

    test(photo): photos_spec.rb

--- spec/controllers/photos_spec.rb
+++ spec/controllers/photos_spec.rb
@@ -1,11 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe TripsController, type: :controller do
+RSpec.describe PhotosController, type: :controller do
   let(:photos_attributes) {attributes_for(:photo, images: images)}
+  let(:user) {FactoryBot.create(:user)}
+
+  before  do
+    sign_in user
+  end
+
   describe 'GET #index' do
     it 'returns a successful response' do
       get :index
       expect(response).to render_template(:index)
     end
+  end
+
+  describe 'GET #new' do
+    it 'renders the new template' do
+      get :new
+      expect(response).to render_template(:new)
+    end
+
   end
 end
