e87edd628b6737df1a96ce9b6ac550201a86e9ea
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-07-01 16:32:56 -0300

    test(factory): checkin_date

--- spec/factories/trips.rb
+++ spec/factories/trips.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :trip do
     association :establishment
     association :guide
+    checkin_date { Faker::Date.between(from: Date.today, to: Date.today + 1.year) }
   end
 end
