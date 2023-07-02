734d0035dc9373115ba2deef9b4d955b0be2c114
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-07-02 13:36:42 -0300

    test: hotelier crud ref #53

--- spec/factories/hoteliers.rb
+++ spec/factories/hoteliers.rb
@@ -3,6 +3,9 @@ FactoryBot.define do
     sequence(:id) { |n| n }
 
     name {Faker::Name.name}
+    cpf {Faker::Number.number( digits: 11)}
+    phone_number {Faker::PhoneNumber}
+    plan {Faker::Number.digit}
   end
 end
 
