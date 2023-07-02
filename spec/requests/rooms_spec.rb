ded94402a89cb22169a819cfa91ee2d1d77c1690
Sergio Oseko <osk.sergio@gmail.com>
2023-07-02 16:17:21 -0300

    test: first request test with user authentication

--- spec/requests/rooms_spec.rb
+++ spec/requests/rooms_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Rooms API', type: :request do
     
     before do
       sign_in user
-    end        
+    end
 
     context 'With valid parameters' do
       it 'Creates a new room' do
@@ -16,10 +16,6 @@ RSpec.describe 'Rooms API', type: :request do
                     description: 'Room / Apto. 301',
                     reference: 'Andar X - Bloco Y' } }
         }.to change(Room, :count).by(1)
-        
-        expect(response).to have_http_status(:created)
-        expect(response_body).to include('Room 301')
-        expect(response_body).to include('2')
       end
     end
   end
