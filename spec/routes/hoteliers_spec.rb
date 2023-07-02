f0dff55398b1faad75a8d309c91ef4687cc89280
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-07-02 15:48:30 -0300

    test: hotelier routes ref #53

--- spec/routes/hoteliers_spec.rb
+++ spec/routes/hoteliers_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe HoteliersController, type: :routing do
-  describe 'routing' do
+  context 'routes' do
     it 'routes to #index' do
       expect(get: '/hoteliers').to route_to('hoteliers#index')
     end
+  end
+
+  it 'routes to #new' do
+    expect(get: '/hoteliers/new').to route_to('hoteliers#new')
   end
 end
