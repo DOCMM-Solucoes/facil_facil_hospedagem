6e7998501b870598800f4a64a13836848fbf4e55
Osiris Mariano <oslimacontato@gmail.com>
2023-07-03 18:47:54 -0300

    test(photo): spec/factories/photos.rb

--- spec/factories/photos.rb
+++ spec/factories/photos.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :photo do
     title { Faker::Lorem.sentence }
     description { Faker::Lorem.sentence }
-    association :images
+    #association :images
   end
 end
