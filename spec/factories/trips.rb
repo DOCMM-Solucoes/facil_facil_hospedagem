12a10112d8f3cb6567d25d3acddf65d77ec6f531
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-07-03 22:53:38 -0300

    test(trip_test): added establishment_id and guide_id in factory

--- spec/factories/trips.rb
+++ spec/factories/trips.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :trip do
-    association :establishment
-    association :guide
+    establishment_id { FactoryBot.create(:establishment).id }
+    guide_id { FactoryBot.create(:guide).id }
     checkin_date { Faker::Date.between(from: Date.today, to: Date.today + 1.year) }
   end
 end
