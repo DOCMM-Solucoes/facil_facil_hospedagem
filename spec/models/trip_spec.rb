379f3d2ef4537c5076158545d925a67177652fa5
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-07-04 15:33:33 -0300

    test: establishment ref #39

--- spec/models/trip_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Trip, type: :model do
-  describe 'associations' do
-    it { should belong_to(:guide) }
-    it { should belong_to(:establishment) }
-  end
-end
