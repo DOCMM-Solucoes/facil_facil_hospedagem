0460b6fc2b85fe8c9183847d712cf77329baf3d3
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-07-07 07:34:41 -0300

    test(guide): association with address in factory

--- spec/factories/guides.rb
+++ spec/factories/guides.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory :guide do
     sequence(:id) { |n| n }
     name { Faker::Name.name }
+
+    after(:build) do |guide|
+      guide.address ||= FactoryBot.build(:address, addressable: guide)
+    end
   end
 end
