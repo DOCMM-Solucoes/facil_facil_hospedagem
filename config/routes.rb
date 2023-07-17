77fcef0a6cc0db099da9b896b1a384f24158e1f5
Marco Castro <marcodotcastro@gmail.com>
2023-07-17 18:12:24 -0300

    fix: route

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   get 'welcome/index'
-  scope '(locale)', locale:/pt-BR|en/ do
-    devise_for :users
-    root 'welcome#index'
-    resources :photos
-    resources :guides
-    resources :establishments
-    resources :hoteliers
-    resources :rooms
-    resources :trips
-  end
+  root 'welcome#index'
+  resources :photos
+  resources :guides
+  resources :establishments
+  resources :hoteliers
+  resources :rooms
+  resources :trips
 end
