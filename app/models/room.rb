cf615cb45a35d508029f73369b42a7beed042e33
Sergio Oseko <osk.sergio@gmail.com>
2023-07-31 22:11:37 -0300

    feat: melhoria e exibição das fotos dos quartos

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,5 @@
 class Room < ApplicationRecord
-	has_many_attached :images, dependent: :destroy
+	has_many_attached :images, dependent: :destroy do |attachable|
+		attachable.variant :thumb, resize: "200x200"
+	end
 end
