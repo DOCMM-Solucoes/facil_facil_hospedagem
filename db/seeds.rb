26870cce237321d1880d0ced67f1eb83f114bf81
Osiris Mariano <oslimacontato@gmail.com>
2023-08-21 10:33:16 -0300

    feat: style seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,11 @@
+=begin
 User.create!(email: 'admin@ffh.com', password: 'abc12345abc', password_confirmation: 'abc12345abc')
+=end
+
+5.times do |i = photo.create|
+  photo = Photo.create!(
+    title: Faker::Lorem.sentence,
+    #image: "https://picsum.photos/800/600",  # URL de imagem do serviço Lorem Picsum
+    description: Faker::Lorem.paragraph
+  )
+end
