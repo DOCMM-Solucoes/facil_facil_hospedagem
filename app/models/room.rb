ccca0ee68243c8b7110752b37a3dfb4882c9fa6e
Sergio Oseko <osk.sergio@gmail.com>
2023-08-31 21:30:01 -0300

    fix: finalizada a correção das validações customizadas e somente números na capacidade máx.

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,37 +1,6 @@
-# == Schema Information
-#
-# Table name: rooms
-#
-#  id                      :bigint           not null, primary key
-#  description             :string
-#  is_couple               :boolean
-#  is_couple_plus_four     :boolean
-#  is_couple_plus_one      :boolean
-#  is_couple_plus_three    :boolean
-#  is_couple_plus_two      :boolean
-#  is_double               :boolean
-#  is_quadruple            :boolean
-#  is_quintuple            :boolean
-#  is_sextuple             :boolean
-#  is_single               :boolean
-#  is_three_couples        :boolean
-#  is_triple               :boolean
-#  is_two_couples          :boolean
-#  is_two_couples_plus_one :boolean
-#  is_two_couples_plus_two :boolean
-#  maximum_capacity        :integer
-#  name                    :string
-#  photo_1                 :string
-#  photo_2                 :string
-#  photo_3                 :string
-#  photo_4                 :string
-#  reference               :string
-#  status                  :string
-#  created_at              :datetime         not null
-#  updated_at              :datetime         not null
-#
 class Room < ApplicationRecord
 	validates :name, :maximum_capacity, :status, presence: true
+	validates :maximum_capacity, numericality: { only_integer: true }
 	validate :at_least_one_accommodation_type_must_be_selected
 	validate :at_least_one_option_single_double
 
@@ -39,17 +8,16 @@ class Room < ApplicationRecord
 		attachable.variant :thumb, resize: "200x200"
 	end
 
+	private
 	def at_least_one_accommodation_type_must_be_selected
-		#return true if :is_double == true || :is_triple == true || :is_quadruple == true || :is_quintuple == true || :is_sextuple == true
-		if :is_double == false and :is_triple == false and :is_quadruple == false and :is_quintuple == false and :is_sextuple == false
-		  errors.add( :is_double, "At least one accommodation type must be selected")
+		unless is_double || is_triple || is_quadruple || is_quintuple || is_sextuple
+		  errors.add( :base, "At least one accommodation type must be selected")
 		end
 	end
 
 	def at_least_one_option_single_double
-		#return true if :is_single == true || :is_couple == true || :is_two_couples == true || :is_three_couples == true
-		if :is_single == false and :is_couple == false and :is_two_couples == false and :is_three_couples == false
-  		errors.add( :is_single,"At least one option must be selected referring to the single or double room")
+		unless is_single || is_couple || is_two_couples || is_three_couples
+  		errors.add( :base,"At least one option must be selected referring to the single or double room")
 		end
 	end
 end
