6bc303772456bdc4a33f9f661f72015b542eec36
Sergio Oseko <osk.sergio@gmail.com>
2023-09-03 11:01:44 -0300

    feat: quarto deve possuir pelo menos uma foto vinculada

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,6 +3,7 @@ class Room < ApplicationRecord
 	validates :maximum_capacity, numericality: { only_integer: true }
 	validate :at_least_one_accommodation_type_must_be_selected
 	validate :at_least_one_option_single_double
+	validate :at_least_one_image_attached
 
 	has_many_attached :images, dependent: :destroy do |attachable|
 		attachable.variant :thumb, resize: "200x200"
@@ -18,6 +19,12 @@ class Room < ApplicationRecord
 	def at_least_one_option_single_double
 		unless is_single || is_couple || is_two_couples || is_three_couples
   		errors.add( :base,"At least one option must be selected referring to the single or double room")
+		end
+	end
+
+	def at_least_one_image_attached
+		if images.blank?
+			errors.add(:base, "At least one picture must be attached to the room")
 		end
 	end
 end
