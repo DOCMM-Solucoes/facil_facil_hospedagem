1bb38f6b869e957a7895b8fa013b20bab687d7b5
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-09-07 13:38:12 -0300

    feat(room): error message translations

--- app/models/room.rb
+++ app/models/room.rb
@@ -12,19 +12,19 @@ class Room < ApplicationRecord
 	private
 	def at_least_one_accommodation_type_must_be_selected
 		unless is_double || is_triple || is_quadruple || is_quintuple || is_sextuple
-		  errors.add( :base, "At least one accommodation type must be selected")
+			errors.add(:base, I18n.t('app.errors.type_of_accommodation'))
 		end
 	end
 
 	def at_least_one_option_single_double
 		unless is_single || is_couple || is_two_couples || is_three_couples
-  		errors.add( :base,"At least one option must be selected referring to the single or double room")
+			errors.add(:base, I18n.t('app.errors.single_or_double'))
 		end
 	end
 
 	def at_least_one_image_attached
 		if images.blank?
-			errors.add(:base, "At least one picture must be attached to the room")
+			errors.add(:base, I18n.t('app.errors.photo attached'))
 		end
 	end
 end
