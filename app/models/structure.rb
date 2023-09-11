5d520153dfb08a226142603ff61eb39211cd4aa0
Osiris Mariano <oslimacontato@gmail.com>
2023-09-11 14:22:01 -0300

    feat: structure_issues

--- app/models/structure.rb
+++ app/models/structure.rb
@@ -13,3 +13,23 @@ class Structure < ApplicationRecord
   has_many_attached :images, dependent: :destroy
   validates :title, :description, :images, presence: true
 end
+
+private
+
+def validate_presence_of(attribute, error_message)
+  unless send(attribute).present?
+    errors.add(:base, i18n.t('errors.#{error_message}'))
+  end
+end
+
+def at_least_one_described_title
+  validate_presence_of(:title, 'type_of_title')
+end
+
+def at_least_a_written_description
+  validate_presence_of(:description, 'type_of_description')
+end
+
+def at_least_one_attached_image
+  validate_presence_of(:images, 'photo_attached')
+end
