81a14be53ff0e7bcba660764fd4cbce88223ff4b
Marco Castro <marcodotcastro@gmail.com>
2023-09-12 14:20:29 -0300

    build: change start.sh

--- bin/render/start.sh
+++ bin/render/start.sh
@@ -2,10 +2,12 @@
 # exit on error
 set -o errexit
 
+rm Gemfile.lock
+gem install rake -v '12.2'
 gem install bundler -v 2.1.4
 gem update --system
 
-bundle update rake
+bundle update
 bundle install
 npm install
 bundle exec rake assets:precompile
