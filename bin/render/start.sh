cf2aa58d193d8cf2367f80fc12ed2893b5823fec
Marco Castro <marcodotcastro@gmail.com>
2023-09-12 11:26:23 -0300

    build: start.sh

---
+++ bin/render/start.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+gem install bundler
+gem update --system
+bundle install
+npm install
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+rails db:migrate
